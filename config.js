886e74eb648c947afe3d43acadb33beadaf2b2e0
KAVIDU-MDV2 <kaviduasehnmdb.ot@gmail.com>
2025-10-24 20:55:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'PUT SESSION ID HERE' : process.env.SESSION_ID, 
+SESSION_ID: process.env.SESSION_ID === undefined ? 'dark~hVZBxDrQ#WggU4QTue9pY5OnCvvxYvnC4I3DcgfXAzbnWZjagmAg' : process.env.SESSION_ID, 
 PORT: process.env.PORT === undefined ? '8000' : process.env.PORT,    
 SESSION_NAME: process.env.SESSION_NAME === undefined ? 'DARK-MD' : process.env.SESSION_NAME, 
 
